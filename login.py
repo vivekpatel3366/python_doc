ec09af77d2d4ba7f5cd70df11e1208aa84bd2a71
VIVEKKUMAR K PAEL <vivekpatel3366@gmail.com>
2025-03-21 16:09:02 +0530

    Update login.py
    okk

--- login.py
+++ login.py
@@ -66,3 +66,125 @@ btn.grid(column=2, row=0)
          
 # insert()
 m.mainloop()
+
+
+
+
+--------------------------
+# tuple = colletion which  is ordered and unchnageable
+#         used to group together related data
+
+# student=("Bro",21,"male")
+
+# print(student.count("Count"))
+# print(student.index("male"))
+
+# for i in student:
+#         print(i)     
+
+# if 21 in student:     
+#         print("Bro is here!")
+
+# len(), min(), max(), tuple(), index(), count(), sum(), and sorted()
+
+
+# =================================
+# set = collecttion which is unorderd, unindexed. No duplicate values
+
+# utensils ={"fork","spoon","knife"}
+# v2={1,2,3}
+# utensils.add("napkin")
+
+# utensils.remove("fork")
+
+# utensils.clear()
+# print(utensils)
+
+# v2=utensils.union(v2)
+
+# print(v2.difference(utensils))
+
+# utensils.update(v2)
+# for x in utensils:
+#         print(x)
+
+
+# =============================================
+# dictionary = A changable, unorderd collection of unque of qnique key:value
+#    pairs Fast beacuse they use hasing ,allow us to access a valur quikly
+
+# capitals = {
+#         'Usa' :'washington dc',
+#         'india':'new delhi',
+#         'china':'beijing',
+#         'russia':'moscow'
+# }
+
+# capitals.update({'germany':'barline'})
+# capitals.update({'Usa':'las veges'})
+
+# capitals.pop('russia')
+
+# capitals.clear()
+
+# # print(capitals['Usa'])
+# print(capitals.get('Germany'))
+# print(capitals.keys())
+# print(capitals.values())
+# print(capitals.items())
+
+# for key,value in capitals.items():
+#         print(key,value)
+
+
+
+#============================================
+# index operator [] = give access to a sequence's element (str,list,tuple)
+
+# name = "bro Code"
+
+# if(name[0].islower()):
+#         name=name.capitalize()
+        
+# first_name=name[0:3].upper()
+# last_name=name[4:].lower()
+# last_character = name[-1]
+
+# print(last_character )``
+# print(name)
+
+
+# ==============================================
+# funtion = a block of code which is executed only when it is called
+
+# def hello(a,b,c):
+#         print("hello:"+a+b)
+#         print("You are "+str(c)+" year old")
+#         print("HAve a nice day")
+        
+# hello("vivek"," patel",23)
+
+
+ # ====================================================
+# return statement =function send Python value/objects back to the caller.
+#    These values/object are knows as the function's return value
+
+# def multiply(number1,number2):
+#         result =number1+number2
+#         return result
+
+# y=multiply(3,5)
+# print(multiply(10,20),y)
+
+
+# =============================================================
+# keyword argument = argument preceded by an identifier when we pass them to a fuction
+                #    The order of the arguments does'nt matter, unlike position arguments
+                #    python knows the names of the arguments that our function recevies
+                
+        
+# def hello(first,middle,last):
+#         print("hello "+first+" "+middle +" last")
+        
+# hello(last='code',middle='Dude',first="BRo")
+        
